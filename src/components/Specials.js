b71a8b9d93b6d80dc7fea80cf3be10bb0af31c88
BalazsGyulai <60194914+BalazsGyulai@users.noreply.github.com>
2023-03-18 16:35:08 +0000

    codespace try

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import S0 from "../Rails/S0"
 import S1 from "../Rails/S1"
 import S2 from "../Rails/S2"
@@ -6,8 +6,10 @@ import S3 from "../Rails/S3"
 import S4 from "../Rails/S4"
 import S5 from "../Rails/S5"
 import "./Specials.css";
+import Moving from "../Rails/Moving"
 
 const Specials = () => {
+    const { SetSelected, selected } = useContext(Moving);
     const SPECIALS = [
         {
             name: "S0",
@@ -35,17 +37,18 @@ const Specials = () => {
         },
     ];
 
-    const chooseItem = (name) => {
-        console.log(name);
+    const chooseItem = (item) => {
+        SetSelected(item);
     }
 
   return (
     <div id="Specials">
         {
             SPECIALS.map((special, index) => (
-                <div key={index} className="SpecialItem" onClick={() => chooseItem(special.name)} >{special.item}</div>
+                <div key={index} className="SpecialItem" onClick={() => chooseItem(special.item)} >{special.item}</div>
             ))
         }
+     {selected}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {MovingManage} from './Rails/Moving'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MovingManage>
+      <App />
+    </MovingManage>
   </React.StrictMode>
 );
 
