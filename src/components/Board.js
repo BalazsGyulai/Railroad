ed520ed656e50bafa7e08a031e3337019d917f6e
= <trombita0531@gmail.com>
2023-03-24 20:04:27 +0100

    is the bug fixed now?

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,34 +4,36 @@ import "./Board.css";
 import BoardTable from "../data/Board";
 
 const Board = () => {
-  const { board } = useContext(BoardTable);
+  const { board, windowSize, cellSize } = useContext(BoardTable);
 
   return (
-    <div id="boardPlace">
-    <div id="board">
-      {board.map((row, y) => (
-        <>
-          <div className="row" key={y}>
-            {row.map((cell, x) =>
-              cell === null ? (
-                <Cell
-                  key={`${y}${x}`}
-                  borderColor="rgb(0, 106, 255)"
-                  properties={cell}
-                  position={{ x: x, y: y }}
-                />
-              ) : (
-                <Cell
-                  key={`${y}${x}`}
-                  properties={cell}
-                  position={{ x: x, y: y }}
-                />
-              )
-            )}
-          </div>
-        </>
-      ))}
-    </div>
+    <div id="boardPlace" style={{
+      height: windowSize.x < 769 ? `${windowSize.y * 0.8 - (cellSize + 10)}px` : `${windowSize.y}`
+    }}>
+      <div id="board">
+        {board.map((row, y) => (
+          <>
+            <div className="row" key={y}>
+              {row.map((cell, x) =>
+                cell === null ? (
+                  <Cell
+                    key={`${y}${x}`}
+                    borderColor="rgb(0, 106, 255)"
+                    properties={cell}
+                    position={{ x: x, y: y }}
+                  />
+                ) : (
+                  <Cell
+                    key={`${y}${x}`}
+                    properties={cell}
+                    position={{ x: x, y: y }}
+                  />
+                )
+              )}
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 };
