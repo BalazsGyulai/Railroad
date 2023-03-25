00552dd0da06323aa789ab50bfa99943d61f05ad
= <trombita0531@gmail.com>
2023-03-25 17:49:37 +0100

    smoother code

--- src/components/Board.js
+++ src/components/Board.js
@@ -39,166 +39,86 @@ const Board = () => {
     if (selected !== "" && selected !== null) {
       for (let y = 0; y < board.length; y++) {
         for (let x = 0; x < board[y].length; x++) {
-          if (board[y][x] !== null) {
-            if (board[y][x].look && selected.look) {
-              // is the top of the placed item equal with the bottom of the selected item but not null
-              // when the rotate value is odd then you should check the top of the placed item with the top of the selected item because the array wont change, just the item rotate with css
+          if (board[y][x] !== null && board[y][x].look && selected.look) {
+            let Placed = {
+              top: board[y][x].look[(0 + ((4 - board[y][x].rotated) % 4)) % 4],
+              right:
+                board[y][x].look[(1 + ((4 - board[y][x].rotated) % 4)) % 4],
+              bottom:
+                board[y][x].look[(2 + ((4 - board[y][x].rotated) % 4)) % 4],
+              left: board[y][x].look[(3 + ((4 - board[y][x].rotated) % 4)) % 4],
+            };
 
+            let Selected = {
+              top: selected.look[(0 + ((4 - selected.rotated) % 4)) % 4],
+              right: selected.look[(1 + ((4 - selected.rotated) % 4)) % 4],
+              bottom: selected.look[(2 + ((4 - selected.rotated) % 4)) % 4],
+              left: selected.look[(3 + ((4 - selected.rotated) % 4)) % 4],
+            };
+
+            // top <-> bottom
+            if (selected.rotated % 2 === 1 && selected.flip === -1) {
+              // flipped -> placed top <-> selected top
               if (y - 1 > 0) {
-                if (selected.rotated % 2 === 1 && selected.flip === -1) {
-                  if (
-                    board[y][x].look[
-                      (0 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] ===
-                      selected.look[(0 + ((4 - selected.rotated) % 4)) % 4] &&
-                    board[y][x].look[
-                      (0 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] !== null &&
-                    selected.look[(0 + ((4 - selected.rotated) % 4)) % 4] !==
-                      null
-                  ) {
-                    EnableToPlace[y - 1][x] = "enable";
-                  } else {
-                    EnableToPlace[y - 1][x] = null;
-                  }
-                } else {
-                  if (
-                    board[y][x].look[
-                      (0 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] ===
-                      selected.look[(2 + ((4 - selected.rotated) % 4)) % 4] &&
-                    board[y][x].look[
-                      (0 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] !== null &&
-                    selected.look[(2 + ((4 - selected.rotated) % 4)) % 4] !==
-                      null
-                  ) {
-                    EnableToPlace[y - 1][x] = "enable";
-                  } else {
-                    EnableToPlace[y - 1][x] = null;
-                  }
+                if (Placed.top === Selected.top) {
+                  EnableToPlace[y - 1][x] = "enable";
                 }
               }
 
-              // is the bottom of the placed item equal with the top of the selected item but not null
-              // when the rotate value is odd then you should check the bottom of the placed item with the bottom of the selected item because the array wont change, just the item rotate with css
-
+              // flipped -> placed bottom <-> selected bottom
               if (y + 1 < board.length) {
-                if (selected.rotated % 2 === 1 && selected.flip === -1) {
-                  if (
-                    board[y][x].look[
-                      (2 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] ===
-                      selected.look[(2 + ((4 - selected.rotated) % 4)) % 4] &&
-                    board[y][x].look[
-                      (2 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] !== null &&
-                    selected.look[(2 + ((4 - selected.rotated) % 4)) % 4] !==
-                      null
-                  ) {
-                    EnableToPlace[y + 1][x] = "enable";
-                  } else {
-                    EnableToPlace[y + 1][x] = null;
-                  }
-                } else {
-                  if (
-                    board[y][x].look[
-                      (2 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] ===
-                      selected.look[(0 + ((4 - selected.rotated) % 4)) % 4] &&
-                    board[y][x].look[
-                      (2 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] !== null &&
-                    selected.look[(0 + ((4 - selected.rotated) % 4)) % 4] !==
-                      null
-                  ) {
-                    EnableToPlace[y + 1][x] = "enable";
-                  } else {
-                    EnableToPlace[y + 1][x] = null;
-                  }
+                if (Placed.bottom === Selected.bottom) {
+                  EnableToPlace[y + 1][x] = "enable";
+                }
+              }
+            } else {
+              // not flipped -> placed top <-> selected bottom
+              if (y - 1 > 0) {
+                if (Placed.top === Selected.bottom) {
+                  EnableToPlace[y - 1][x] = "enable";
                 }
               }
 
-              // is the right side of the placed item equal with the left side of the selected item but not null
-              // when the rotate value is even then you should check the right side of the placed item with the right side of the selected item because the array wont change, just the item rotate with css
-
-              if (x + 1 < board[y].length) {
-                if (selected.rotated % 2 === 0 && selected.flip === -1) {
-                  // flipped
-                  if (
-                    board[y][x].look[
-                      (1 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] ===
-                      selected.look[(1 + ((4 - selected.rotated) % 4)) % 4] &&
-                    board[y][x].look[
-                      (1 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] !== null &&
-                    selected.look[(1 + ((4 - selected.rotated) % 4)) % 4] !==
-                      null
-                  ) {
-                    EnableToPlace[y][x + 1] = "enable";
-                  } else {
-                    EnableToPlace[y][x + 1] = null;
-                  }
-                } else {
-                  // not flipped
-                  if (
-                    board[y][x].look[
-                      (1 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] ===
-                      selected.look[(3 + ((4 - selected.rotated) % 4)) % 4] &&
-                    board[y][x].look[
-                      (1 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] !== null &&
-                    selected.look[(3 + ((4 - selected.rotated) % 4)) % 4] !==
-                      null
-                  ) {
-                    EnableToPlace[y][x + 1] = "enable";
-                  } else {
-                    EnableToPlace[y][x + 1] = null;
-                  }
+              // not flipped -> placed bottom <-> selected top
+              if (y + 1 < board.length) {
+                if (Placed.bottom === Selected.top) {
+                  EnableToPlace[y + 1][x] = "enable";
                 }
               }
+            }
 
-              // is the left side of the placed item equal with the right side of the selected item but not null
-              // when the rotate value is even then you should check the left side of the placed item with the left side of the selected item because the array wont change, just the item rotate with css
+            // left <-> right
+            if (selected.rotated % 2 === 0 && selected.flip === -1) {
+              // flipped -> placed left <-> selected left
               if (x - 1 > 0) {
-                if (selected.rotated % 2 === 0 && selected.flip === -1) {
-                  if (
-                    board[y][x].look[
-                      (3 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] ===
-                      selected.look[(3 + ((4 - selected.rotated) % 4)) % 4] &&
-                    board[y][x].look[
-                      (3 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] !== null &&
-                    selected.look[(3 + ((4 - selected.rotated) % 4)) % 4] !==
-                      null
-                  ) {
-                    EnableToPlace[y][x - 1] = "enable";
-                  } else {
-                    EnableToPlace[y][x - 1] = null;
-                  }
-                } else {
-                  if (
-                    board[y][x].look[
-                      (3 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] ===
-                      selected.look[(1 + ((4 - selected.rotated) % 4)) % 4] &&
-                    board[y][x].look[
-                      (3 + ((4 - board[y][x].rotated) % 4)) % 4
-                    ] !== null &&
-                    selected.look[(1 + ((4 - selected.rotated) % 4)) % 4] !==
-                      null
-                  ) {
-                    EnableToPlace[y][x - 1] = "enable";
-                  } else {
-                    EnableToPlace[y][x - 1] = null;
-                  }
+                if (Placed.left === Selected.left) {
+                  EnableToPlace[y][x - 1] = "enable";
+                }
+              }
+
+              // flipped -> placed right <-> selected right
+              if (x + 1 < board[y].length) {
+                if (Placed.right === Selected.right) {
+                  EnableToPlace[y][x + 1] = "enable";
+                }
+              }
+            } else {
+              // not flipped -> placed left <-> selected right
+              if (x - 1 > 0) {
+                if (Placed.left === Selected.right) {
+                  EnableToPlace[y][x - 1] = "enable";
+                }
+              }
+
+              // not flipped -> placed right <-> selected left
+              if (x + 1 < board[y].length) {
+                if (Placed.right === Selected.left) {
+                  EnableToPlace[y][x + 1] = "enable";
                 }
               }
             }
           }
+
         }
       }
     }
